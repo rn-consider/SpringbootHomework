ca5d51e196e6a5e6aea5d49676c24b7567fa992b
rn-consider <3214858229@qq.com>
2023-05-21 22:05:48 +0800

    任务3

--- src/main/java/com/example/lowversion/config/SecurityConfig.java
+++ src/main/java/com/example/lowversion/config/SecurityConfig.java
@@ -1,19 +1,27 @@
 package com.example.lowversion.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//密码编码器
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//内存身份认证，模拟测试用户
-        auth.inMemoryAuthentication().passwordEncoder(encoder)
-                .withUser("shitou").password(encoder.encode("123456")).roles("common")
-                .and()
-                .withUser("李四").password(encoder.encode("123456")).roles("vip");
+        String userSQL1 ="select username,password,valid from t_customer "+ "where username = ?";
+        String authoritySQL="select c.username,a.authority from t_customer c, t_authority a, t_customer_authority ca where ca.customer_id=c.id and ca.authority_id=a.id and c.username =?";
+        auth.jdbcAuthentication().passwordEncoder(encoder).dataSource(dataSource)
+                .usersByUsernameQuery(userSQL1).authoritiesByUsernameQuery(authoritySQL);
     }}
+
+
