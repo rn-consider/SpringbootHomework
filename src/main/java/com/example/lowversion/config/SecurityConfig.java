00a94ac576ee2597503c342fca46a6d928d1b0c2
rn-consider <3214858229@qq.com>
2023-05-23 16:35:40 +0800

    任务八

--- src/main/java/com/example/lowversion/config/SecurityConfig.java
+++ src/main/java/com/example/lowversion/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.lowversion.config;
 
 import com.example.lowversion.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.sql.DataSource;
 import java.util.List;
@@ -18,6 +21,14 @@ import java.util.Map;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jr=new JdbcTokenRepositoryImpl();
+        jr.setDataSource(dataSource);
+        return jr;
+    }
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
     @Override
@@ -42,5 +53,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.logout()
                 .logoutUrl("/mylogout")
                 .logoutSuccessUrl("/");
+        http.rememberMe()
+                .rememberMeParameter("rememberme")
+                .tokenValiditySeconds(200)
+                .tokenRepository(tokenRepository());
+
+
     }
+
+
 }
--- src/main/java/com/example/lowversion/controller/FilmController.java
+++ src/main/java/com/example/lowversion/controller/FilmController.java
@@ -1,8 +1,13 @@
 package com.example.lowversion.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FilmController {
@@ -15,5 +20,16 @@ public class FilmController {
     @GetMapping("/userLogin")
     public String toLoginPage() {
         return "login/login";
+    }
+    @GetMapping("/getuserByContext")
+    @ResponseBody
+    public void getUser2() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        System.out.println("userDetails: "+context);
+        Authentication authentication = context.getAuthentication();
+        UserDetails principal =
+                (UserDetails)authentication.getPrincipal();
+        System.out.println(principal);
+        System.out.println("username: "+principal.getUsername());
     }
 }
