64f93be98e4f4b5c91418ac0dae0528a8c15071a
rn-consider <3214858229@qq.com>
2023-05-23 22:28:23 +0800

    任务1

--- src/test/java/com/example/lowversion/LowversionApplicationTests.java
+++ src/test/java/com/example/lowversion/LowversionApplicationTests.java
@@ -1,10 +1,12 @@
 package com.example.lowversion;
 
+import com.example.lowversion.entity.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +14,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 class LowversionApplicationTests {
 	@Autowired
 	private AmqpAdmin amqpAdmin;
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	@Test
+	public void psubPublisher() {
+		Person person=new Person();
+		person.setAge(1);
+		person.setName("shitou");
+//第一个参数表示定义的交换器名称，第二个表示路由键，第三个表示消息内容
+		rabbitTemplate.convertAndSend("fanout_exchange"
+				,
+				""
+				,person);
+	}
 	@Test
 	public void amqpAdmin() {
 //定义fanout类型的交换器
