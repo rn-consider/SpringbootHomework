a1ea98ddb583f0856311944ae92b2cdf1e3c4b75
rn-consider <3214858229@qq.com>
2023-05-24 00:40:40 +0800

    任务5+6

--- src/main/java/com/example/lowversion/service/SendEmailService.java
+++ src/main/java/com/example/lowversion/service/SendEmailService.java
@@ -2,10 +2,18 @@ package com.example.lowversion.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class SendEmailService{
@@ -20,4 +28,35 @@ public class SendEmailService{
         try { mailSender.send(message);System.out.println("纯文本邮件发送成功");
         } catch (MailException e) {
             System.out.println("纯文本邮件发送失败 "+e.getMessage());e.printStackTrace();}
-    }}
+    }
+    public void sendComplexEmail(String to,String subject,String text,String filePath,String rscId,
+                                 String rscPath){
+        MimeMessage message = mailSender.createMimeMessage();try {
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);helper.setTo(to);
+        helper.setSubject(subject);helper.setText(text, true);
+        FileSystemResource res = new FileSystemResource(new File(rscPath));
+        helper.addInline(rscId, res);
+        FileSystemResource file = new FileSystemResource(new File(filePath));
+        String fileName = filePath.substring(filePath.lastIndexOf(File.separator)+1);
+        helper.addAttachment(MimeUtility.encodeText(fileName), file);
+        mailSender.send(message);
+        System.out.println("复杂邮件发送成功");
+    } catch (MessagingException e) {
+        System.out.println("复杂邮件发送失败 "+e.getMessage());e.printStackTrace();
+    } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void sendTemplateEmail(String to, String subject, String content) {
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);helper.setTo(to);
+            helper.setSubject(subject);helper.setText(content, true);
+            mailSender.send(message);System.out.println("模板邮件发送成功");
+        } catch (MessagingException e)
+        {System.out.println("模板邮件发送失败 "+e.getMessage());
+            e.printStackTrace();}}
+
+}
--- src/test/java/com/example/lowversion/LowversionApplicationTests.java
+++ src/test/java/com/example/lowversion/LowversionApplicationTests.java
@@ -11,6 +11,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 
 @RunWith(SpringRunner.class)
@@ -20,9 +22,36 @@ public class LowversionApplicationTests {
 	private SendEmailService sendEmailService;
 	@Test
 	public void sendSimpleMailTest() {
-		String to="123151231@qq.com";
+		String to="1111111111@qq.com";
 		String subject="【纯文本邮件】标题";
 		String text="Spring Boot纯文本邮件发送内容测试.....";
 		sendEmailService.sendSimpleEmail(to,subject,text);
 	}
+	@Test
+	public void sendComplexEmailTest() {
+		String to="1111111111@qq.com";
+		String subject="【复杂邮件】标题";
+		StringBuilder text = new StringBuilder();
+		text.append("<html><head></head>");
+		text.append("<body><h1>祝大家元旦快乐！</h1>");
+		String rscId = "img001";
+		text.append("<img src='cid:" +rscId+"'/></body>");text.append("</html>");
+		String rscPath="C:\\newyear.jpg";
+		String filePath="C:\\text.txt";
+		sendEmailService.sendComplexEmail(to,subject,text.toString(),
+				filePath,rscId,rscPath);}
+	@Autowired
+	private TemplateEngine templateEngine;
+	@Test
+	public void sendTemplateEmailTest() {
+		String to="1111111111@qq.com";
+		String subject="【模板邮件】标题";
+		Context context = new Context();
+		context.setVariable("username", "石头");
+		context.setVariable("code", "456123");
+		String emailContent = templateEngine.process("emailTemplate_vercode",
+				context);
+		sendEmailService.sendTemplateEmail(to,subject,emailContent);}
+
+
 }
