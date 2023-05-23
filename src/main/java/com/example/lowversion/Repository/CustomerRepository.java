ace46e516769e88c3eea120ba547ba1e9f39b3ea
rn-consider <3214858229@qq.com>
2023-05-23 16:02:15 +0800

    任务5

---
+++ src/main/java/com/example/lowversion/Repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.example.lowversion.Repository;
+
+import com.example.lowversion.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+    Customer findByUsername(String username);
+}
--- src/main/java/com/example/lowversion/config/SecurityConfig.java
+++ src/main/java/com/example/lowversion/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.example.lowversion.config;
 
+import com.example.lowversion.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
@@ -14,14 +19,21 @@ import java.util.Map;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private DataSource dataSource;
+    private UserDetailsServiceImpl userDetailsService;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String userSQL1 ="select username,password,valid from t_customer "+ "where username = ?";
-        String authoritySQL="select c.username,a.authority from t_customer c, t_authority a, t_customer_authority ca where ca.customer_id=c.id and ca.authority_id=a.id and c.username =?";
-        auth.jdbcAuthentication().passwordEncoder(encoder).dataSource(dataSource)
-                .usersByUsernameQuery(userSQL1).authoritiesByUsernameQuery(authoritySQL);
-    }}
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/detail/common/**").hasAuthority("common")
+                .antMatchers("/detail/vip/**").hasAuthority("vip")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
 
-
+}
