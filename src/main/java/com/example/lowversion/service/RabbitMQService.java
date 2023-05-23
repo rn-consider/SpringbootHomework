7fe2fd6e04c93ade1b110f478d5d9bf43df85427
rn-consider <3214858229@qq.com>
2023-05-23 22:52:04 +0800

    任务4,可能有问题?

--- src/main/java/com/example/lowversion/service/RabbitMQService.java
+++ src/main/java/com/example/lowversion/service/RabbitMQService.java
@@ -1,20 +1,26 @@
 package com.example.lowversion.service;
 
+import com.example.lowversion.entity.Person;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQService{
-    @RabbitListener(queues = "fanout_queue_email")
-    public void psubConsumerEmail(Message message) {
-        byte[] body = message.getBody();
-        String s = new String(body);
-        System.out.println("邮件业务接收到消息： "+s);}
-    @RabbitListener(queues = "fanout_queue_sms")
-    public void psubConsumerSms(Message message) {
-        byte[] body = message.getBody();
-        String s = new String(body);
-        System.out.println("短信业务接收到消息： "+s);
+    @RabbitListener(bindings =@QueueBinding(value = @Queue("fanout_queue_email"), exchange = @Exchange(value = "fanout_exchange"
+            ,type = "fanout")))
+    public void psubConsumerEmailAno(Person user) {
+        System.out.println("邮件业务接收到消息： "+user);
     }
+
+
+    @RabbitListener(bindings =@QueueBinding(value = @Queue("fanout_queue_sms"),exchange = @Exchange(value = "fanout_exchange"
+            ,type = "fanout")))
+    public void psubConsumerSmsAno(Person user) {
+        System.out.println("短信业务接收到消息： "+user);
+    }
+
 }
